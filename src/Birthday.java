0fa75e4b51a60727ef43bca1d9c38305936b119c
StealthMafia <34491758+StealthMafia@users.noreply.github.com>
2018-01-18 13:34:55 -0800

    New Change with comments
    Comented changes

--- src/Birthday.java
+++ src/Birthday.java
@@ -1,25 +1,25 @@
-public class Birthday {  
 	import java.util.*; // to use Scanner
+public class Birthday {  
 
 	public static void main(String[] args) {
 		System.out.print( countFactors(24) );
+		// Birthday Fact Here
 
 	}
-	public static int countFactors(int baseNumber) {
+	public static int countFactors(int baseNumber) { // Replace countFactors with a birthday int 
 
 		int totalFactors = 0;  //cumulative sum
-		for (int i = 1; i <= baseNumber; i++) {
+		for (int i = 1; i <= baseNumber; i++) { // baseNumber needs to be switched to another birthday int 
 
 			if (baseNumber % i == 0) {
-				totalFactors += 1;	// totalFactors = totalFactors + 1
+				totalFactors += 1;	// totalFactors = totalFactors + 1  
 			}
 			else  {
 
 			}
 
 		}
-		return totalFactors; 
-
+		return totalFactors; // Possibly reutnr another int 
 
 		
 	}
